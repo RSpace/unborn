ec025bc181b8d929c927275e56934da015b8c244
Casper Fabricius <me@casperfabricius.com>
2016-01-23 16:06:45 +0100

    Added samurai health and explosions

---
+++ Assets/Scripts/SamuraiHealthController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SamuraiHealthController : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+
+	int currentHealth = 100;
+
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if(collision.gameObject.tag == "FriendlyFire")
+		{
+			// Add explosion
+			var explosion = GameObject.Instantiate(explosionPrefab);
+			explosion.transform.position = collision.gameObject.transform.position;
+
+			// Remove fireball
+			Destroy(collision.gameObject);
+
+			// Subtract health
+			int healthLost = getHealthCost(collision.gameObject.name);
+			currentHealth -= healthLost;
+
+			// Check for death
+			if (currentHealth <= 0) {
+				var samuraiExplosion = GameObject.Instantiate(explosionPrefab);
+				samuraiExplosion.transform.position = collision.gameObject.transform.position;
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	int getHealthCost(string name) {
+		switch (name) {
+		case "MobileSun":
+			return 50;
+		default:
+			return 0;
+		}
+	}
+}
