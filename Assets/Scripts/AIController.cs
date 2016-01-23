413998335b439aa3497d45ff618cde32fe1f0e92
Johan Kallberg <johan.kallberg88@gmail.com>
2016-01-23 13:38:52 +0100

    Initial ai stuff

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+
+
+    public Transform[] spawnPoints;
+
+    public GameObject Agent0Prefab;
+
+    public GameObject[] ActiveAgents;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Spawn");
+	}
+
+    IEnumerable Spawn()
+    {
+        for(int i = 0; i<5; i++)
+        {
+
+            Instantiate(Agent0Prefab, spawnPoints[0].position, spawnPoints[0].rotation);
+            yield return new WaitForSeconds(6.0f);
+        }
+        yield return 0; 
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
