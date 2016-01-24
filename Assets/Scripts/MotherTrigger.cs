f76e8016c7e3d9cfcc12fd83720f4c9f13837a2e
peter <peter.pfisher1@gmail.com>
2016-01-24 15:26:59 +0100

    updates

--- Assets/Scripts/MotherTrigger.cs
+++ Assets/Scripts/MotherTrigger.cs
@@ -1,52 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof(BoxCollider))]
-[RequireComponent (typeof(AudioSource))]
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(AudioSource))]
 public class MotherTrigger : MonoBehaviour
 {
 
-	public AudioClip[] sounds;
-	public GameObject dieEffectPrefab;
+    public AudioClip[] sounds;
+    public GameObject dieEffectPrefab;
 
-	// Use this for initialization
-	private int hp;
-	AudioSource audio;
+    // Use this for initialization
+    private int hp;
+    AudioSource audio;
 
-	void Start ()
-	{
-		hp = sounds.Length;
-		audio = gameObject.GetComponent<AudioSource> ();
-	}
+    void Start()
+    {
+        hp = sounds.Length;
+        audio = gameObject.GetComponent<AudioSource>();
+    }
 
-	void OnTriggerEnter (Collider other)
-	{
+    void OnTriggerEnter(Collider other)
+    {
         if (other.tag != "enemy")
             return;
 
-        Animator agentAnim = (other.gameObject.GetComponent<Animator> ());
-		if (agentAnim != null)
-			agentAnim.SetTrigger ("attack");
-       
-		TakeDamage ();
-	}
+        Animator agentAnim = (other.gameObject.GetComponent<Animator>());
+        if (agentAnim != null)
+            agentAnim.SetTrigger("attack");
 
-	private void TakeDamage ()
-	{
-		if (hp <= 0)
-			return;
-			
-		audio.clip = sounds [sounds.Length - hp];
-		audio.Play ();
-		hp--;
-		if (hp == 0)
-			Die ();
-	}
+        TakeDamage();
+    }
 
-	private void Die ()
-	{
-		var dieEffect = GameObject.Instantiate (dieEffectPrefab);
-		dieEffect.transform.position = gameObject.transform.position;
-		//Destroy (gameObject);			
-	}
+    private void TakeDamage()
+    {
+        if (hp <= 0)
+            return;
+
+        audio.clip = sounds[sounds.Length - hp];
+        audio.Play();
+        hp--;
+        if (hp == 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        var dieEffect = GameObject.Instantiate(dieEffectPrefab);
+        dieEffect.transform.position = gameObject.transform.position;
+        StartCoroutine(WaitAndPrint());
+        //Destroy (gameObject);			
+    }
+
+
+    IEnumerator WaitAndPrint()
+    {
+        yield return new WaitForSeconds(3);
+        SteamVR_Fade.View(Color.clear, 0);
+        SteamVR_Fade.View(Color.red, 3);
+        yield return new WaitForSeconds(3);
+        Application.LoadLevel(0);
+    }
 }
